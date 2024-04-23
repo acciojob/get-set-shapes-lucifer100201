ddb2599e93fa48422810ab9d5fef443e9e775fec
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-04-23 02:34:07 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width = width;
+		this.height = height;
+	}
 
-class Square extends Animal {}
+	get width(){
+		return this._width;
+	}
+
+	get height(){
+		return this._height;
+	}
+
+	set width(widthValue){
+		this._width = widthValue;
+	}
+
+	set height(heightValue){
+		this._height = heightValue;
+	}
+
+	getArea(){
+		let area = this._width * this._height;
+		return area;
+	}
+}
+
+class Square extends Animal {
+	constructor(side){
+		this.side =side;
+	}
+
+	getPerimeter(){
+		let perimeter = 2 * side ;
+		return perimeter;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
